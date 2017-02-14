d858d4394cfbc90c0f3f0bd1eae3628ae7bfdbb1
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-02-14 16:24:58 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,23 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    total = 0
+    items.each do |item|
+      total += item.price
+    end
+    total
+  end
+
+  def add_item(item_id)
+    line_item = LineItem.find_by(item_id: item_id, cart_id: self.id)
+    if line_item.nil?
+      line_item = self.line_items.new(item_id: item_id)
+    end
+    line_item
+  end
+
 
 end
